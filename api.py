531cf186363341d753dcd01476ff6f8ec8018a9d
Axel S. Toro Vega <axelsariel@gmail.com>
2020-10-17 05:48:14 +0000

    AFlask app now accessible by external IP

--- api.py
+++ api.py
@@ -1,10 +1,10 @@
 import flask
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+app.config["DEBUG"] = False
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Hello world!</h1>"
 
-app.run()
+app.run(host='0.0.0.0', port=5000)
